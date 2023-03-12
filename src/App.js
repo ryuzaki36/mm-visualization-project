f2423096fc565ff3c5aaf4a2a6d9250fa521b0dc
aayush262 <aayushlama26@gmail.com>
2023-03-13 01:37:30 +0545

    dashbaord page

--- src/App.js
+++ src/App.js
@@ -1,37 +1,10 @@
-import React from 'react';
-import './App.css';
+import Dashboard from "./pages/Dashboard";
+import { Landing } from "./pages/Landing";
 
-function Navbar() {
-  return (
-    <nav className="bg-gray-800">
-      <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
-        <div className="relative flex items-center justify-between h-16">
-          <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
-            <div className="flex-shrink-0">
-              <h1 className="text-white font-bold">My App</h1>
-            </div>
-            <div className="hidden sm:block sm:ml-6">
-              <div className="flex space-x-4">
-                <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Home</a>
-                <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">About</a>
-                <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Contact</a>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </nav>
-  );
-}
 
 function App() {
   return (
-    <div className="bg-gray-200 min-h-screen">
-      <Navbar />
-      <div className="container mx-auto px-4 py-8">
-        <p className="text-gray-700">Hello, Tailwind CSS!</p>
-      </div>
-    </div>
+  <Dashboard />
   );
 }
 
---
+++ src/pages/Landing.jsx
@@ -0,0 +1,4 @@
+export const Landing = () => {
+  return 'hel'
+  
+}
